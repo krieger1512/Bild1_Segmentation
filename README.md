3f7659399dca5ab51a18a260b5143e02d18c3bc9
Steele <minh.nguyen4@stud.fra-uas.de>
2024-06-09 00:39:35 +0200

    zwischenupdate

--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 
 ## Problem Definition
 
-Given an image with recognizable shapes, divide the image into segments so each segment represents one shape.
+Given an image with objects of recognizable shapes, divide the image into segments so each segment represents one shape.
 
 This work aims to solve simple image segmentation task with traditional computer vision algorithms instead of machine learning models.
+
+
 
 We will break down this problems into smaller tasks:
 1. Read the Image: Load the image using OpenCV.
 2. Convert the Image to Grayscale: Convert the image from color (BGR) to grayscale.
 3. Apply Gaussian Blur: Apply Gaussian blur to smooth the image and reduce noise.
-4. Threshold the Image: Apply a binary threshold to create a binary image where the foreground (woman) is white, and the background is black.
-5. Find Contours: Use the findContours function to detect the contours in the thresholded image.
-6. Draw Contours: Draw the detected contours on the original image or on a blank image to visualize the segmentation.
+4. Watershed algorithm
+5. GrabCut algorithm
+6. Find Contours: Use the findContours function to detect the contours in the thresholded image.
+7. Draw Contours: Draw the detected contours on the original image or on a blank image to visualize the segmentation.
+8. Color the segments
 
 ## Setup/Preconfiguration
 
--- segmenter.py
+++ segmenter.py
@@ -1,41 +1,84 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-# Step 1: Read the image
-image = cv2.imread('path_to_your_image.jpg')
 
-# Step 2: Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def opencv_resize(image, ratio):
+    width = int(image.shape[1] * ratio)
+    height = int(image.shape[0] * ratio)
+    dim = (width, height)
+    return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
-# Step 3: Apply Gaussian Blur
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# Step 4: Apply Thresholding
-_, thresh = cv2.threshold(blurred, 128, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+def plot_gray(image):
+    plt.figure(figsize=(16, 10))
+    return plt.imshow(image, cmap="Greys_r")
 
-# Step 5: Find Contours
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Step 6: Draw Contours
-# Create an empty image to draw contours
-contour_image = np.zeros_like(image)
-cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
+def plot_rgb(image):
+    plt.figure(figsize=(16, 10))
+    return plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-# Optionally, draw contours on the original image
-contour_image_on_original = image.copy()
-cv2.drawContours(contour_image_on_original, contours, -1, (0, 255, 0), 2)
 
-# Display the results
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.title('Contours on Blank Image')
-plt.imshow(cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB))
-plt.axis('off')
+# approximate the contour by a more primitive polygon shape
+def approximate_contour(contour):
+    peri = cv2.arcLength(contour, True)
+    return cv2.approxPolyDP(contour, 0.032 * peri, True)
 
-plt.subplot(1, 2, 2)
-plt.title('Contours on Original Image')
-plt.imshow(cv2.cvtColor(contour_image_on_original, cv2.COLOR_BGR2RGB))
-plt.axis('off')
 
-plt.show()
+if __name__ == "__main__":
+
+    # Step 1: Import the image
+    IMAGE_PATH = os.path.join(os.getcwd(), "input", "art.jpg")
+    image = cv2.imread(IMAGE_PATH)
+    original = image.copy()
+    image = opencv_resize(
+        image, ratio=500 / image.shape[0]
+    )  # Downscale image as finding contour is more efficient on a small image
+
+    # In order to find object contour, standard edge detection preprocessing is applied:
+    # Step 2: Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    plot_gray(gray)
+
+    # Step 3: Apply Gaussian Blur
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # plot_gray(blurred)
+
+    # Morphological Transformations: Opening
+    # Our objects are black, so we want to remove white noise
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
+    opened = cv2.morphologyEx(blurred, cv2.MORPH_OPEN, rectKernel)
+    plot_gray(opened)
+
+    # Apply Canny edge detection
+    # Link: https://docs.opencv.org/4.x/da/d22/tutorial_py_canny.html
+    edged = cv2.Canny(opened, 100, 200, apertureSize=3)
+    plot_gray(edged)
+
+    # # Apply thresholding
+    # _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # plot_gray(thresh)
+
+    # # Combine edges and thresholded image
+    # combined = cv2.bitwise_or(edged, thresh)
+    # plot_gray(combined)
+
+    # # For better accuracy, use binary images. So before finding contours, apply threshold or canny edge detection.
+    # # Detect all contours in Canny-edged image
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contour = cv2.convexHull(contours)
+    # print([cv2.contourArea(cnt) for cnt in contours])
+    # contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 10]
+
+    image_with_contours = cv2.drawContours(image.copy(), contours, -1, (0, 255, 0), 2)
+    plot_rgb(image_with_contours)
+
+    # # Get 10 largest contours
+    # largest_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    # image_with_largest_contours = cv2.drawContours(
+    #     image.copy(), largest_contours, -1, (0, 255, 0), 2
+    # )
+    # plot_rgb(image_with_largest_contours)
+    plt.show()
